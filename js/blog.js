a743604c22f4a112e81c167d4b9e11445c7e2563
Stevo5o <stevo.joc@gmail.com>
2015-03-24 11:07:36 +0000

    Displaying blogs from DB

--- js/blog.js
+++ js/blog.js
@@ -3,10 +3,31 @@ $(function() {
 
 	Parse.initialize("769EVCQQ8bpBBDeHfXR4E4i77L8u5uvMksRsUWee", "4TCWeKtnZ91KAJ0UOI60S8cdVddUH3sIHoLOkS6y");
 
-	var TestObject = Parse.Object.extend("TestObject");
-	var testObject = new TestObject();
-	testObject.save({foo: "bar"}).then(function(object) {
-		alert("yep working");
+	var Blog = Parse.Object.extend("Blog");
+	var Blogs = Parse.Collection.extend({
+		model: Blog
+	});
+	var blogs = new Blogs();
+
+	var BlogsView = Parse.View.extend({
+		template: Handlebars.compile($('#blogs-tpl').html()),
+		render: function() {
+			var collection = {
+				blog: this.collection.toJSON()
+			};
+			this.$el.html(this.template(collection));
+		}
+	});
+	blogs.fetch({
+		success: function(blogs) {
+			var blogsView = new BlogsView({
+				collection: blogs
+			});
+			blogsView.render();
+			$('.main-container').html(blogsView.el);
+		},
+		error: function(blogs, error) {
+			console.log(error);
+		}
 	});
 });
-
