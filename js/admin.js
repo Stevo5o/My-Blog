fb959f799f49a2bdff6ee1dcf574798310688c5e
Stevo5o <stevo.joc@gmail.com>
2015-03-25 18:43:08 +0000

    Create templates

--- js/admin.js
+++ js/admin.js
@@ -25,6 +25,50 @@ $(function() {
             }
         });
 
+        // define views
+        var LoginView = Parse.View.extend({
+            template: Handlebars.compile($('#login-tpl').html()),
+            events: {
+                'submit .form-signin': 'login'
+            },
+            login: function(e) {
+                // prevent default submit event
+                e.preventDefault();
+
+                // get data from th form and put them inot vars
+                var data = $(e.target).serializeArray(),
+                    username = data[0].value,
+                    password = data[1].value;
+
+                // call parse login function with those vars
+                Parse.User.logIn(username, password, {
+                    // if th username and password matches
+                    success: function(user, error) {
+                        alert('Welcome!');
+                    },
+                    // if there is an error
+                    error: function(user, error) {
+                        console.log(error);
+                    }
+                });
+            },
+            render: function() {
+                this.$el.html(this.template());
+            }
+        }),
+
+        WelcomeView = Parse.View.extend({
+            template: Handlebars.compile($('#welcome-tpl').html()),
+            render: function() {
+                var attributes = this.model.toJson();
+                this.$el.html(this.template(attributes));
+            }
+        });
     });
+
+		// render login view 
+        var loginView = new LoginView();
+        loginView.render();
+        $('.main-container').html(loginView.el);
 
 });
