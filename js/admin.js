87de5f13d33050bc695b89dc8592d61136d7acfe
Stevo5o <stevo.joc@gmail.com>
2015-03-26 09:48:00 +0000

    Render Welcome View in the Success Callback

--- js/admin.js
+++ js/admin.js
@@ -1,48 +1,50 @@
-$(function () {
+$(function() {
     Parse.$ = jQuery;
 
     Parse.initialize("769EVCQQ8bpBBDeHfXR4E4i77L8u5uvMksRsUWee", "4TCWeKtnZ91KAJ0UOI60S8cdVddUH3sIHoLOkS6y");
-        // define views
-        var LoginView = Parse.View.extend({
+    // define views
+    var LoginView = Parse.View.extend({
             template: Handlebars.compile($('#login-tpl').html()),
             events: {
                 'submit .form-signin': 'login'
             },
-            login: function (e) {
+            login: function(e) {
 
                 // prevent default submit event
                 e.preventDefault();
 
                 // get data from th form and put them inot vars
                 var data = $(e.target).serializeArray(),
-                        username = data[0].value,
-                        password = data[1].value;
+                    username = data[0].value,
+                    password = data[1].value;
 
                 // call parse login function with those vars
                 Parse.User.logIn(username, password, {
                     // if th username and password matches
-                    success: function (user, error) {
-                        alert('Welcome!');
+                    success: function(user) {
+                        var welcomeView = new WelcomeView({ model: user });
+                        welcomeView.render();
+                        $('.main-container').html(welcomeView.el);
                     },
                     // if there is an error
-                    error: function (user, error) {
+                    error: function(user, error) {
                         console.log(error);
                     }
                 });
             },
-            render: function () {
+            render: function() {
                 this.$el.html(this.template());
             }
         }),
-                WelcomeView = Parse.View.extend({
-                    template: Handlebars.compile($('#welcome-tpl').html()),
-                    render: function () {
-                        var attributes = this.model.toJson();
-                        this.$el.html(this.template(attributes));
-                    }
-                });  
+        WelcomeView = Parse.View.extend({
+            template: Handlebars.compile($('#welcome-tpl').html()),
+            render: function() {
+                var attributes = this.model.toJSON();
+                this.$el.html(this.template(attributes));
+            }
+        });
 
-    // Render login view on page
+    // render login view on page
     var loginView = new LoginView();
     loginView.render();
     $('.main-container').html(loginView.el);
