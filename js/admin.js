a71b052e9a22b96c72ce8d53ae5649c6197431ba
Stevo5o <stevo.joc@gmail.com>
2015-03-27 14:01:05 +0000

    Submit blog to parse

--- js/admin.js
+++ js/admin.js
@@ -2,6 +2,24 @@ $(function() {
     Parse.$ = jQuery;
 
     Parse.initialize("769EVCQQ8bpBBDeHfXR4E4i77L8u5uvMksRsUWee", "4TCWeKtnZ91KAJ0UOI60S8cdVddUH3sIHoLOkS6y");
+
+    var Blog = Parse.Object.extend('Blog', {
+        create: function(title, content) {
+            this.set({
+                'title': title,
+                'content': content,
+                'author': Parse.User.current()
+            }).save(null, {
+                success: function(blog) {
+                    alert('you added a new blog: ' + blog.get('title'));
+                },
+                error: function(blog, error) {
+                    console.log(blog);
+                    console.log(error);
+                }
+            });
+        }
+    });
     // define views
     var LoginView = Parse.View.extend({
             template: Handlebars.compile($('#login-tpl').html()),
@@ -22,7 +40,9 @@ $(function() {
                 Parse.User.logIn(username, password, {
                     // if th username and password matches
                     success: function(user) {
-                        var welcomeView = new WelcomeView({ model: user });
+                        var welcomeView = new WelcomeView({
+                            model: user
+                        });
                         welcomeView.render();
                         $('.main-container').html(welcomeView.el);
                     },
@@ -52,17 +72,30 @@ $(function() {
             }
         });
 
+    // blog view
+    var AddBlogView = Parse.View.extend({
+        template: Handlebars.compile($('#add-tpl').html()),
+        events: {
+            'submit .form-add': 'submit'
+        },
+        submit: function(e) {
+            // prevent default submit
+            e.preventDefualt();
+            // take the form and put it into a data object
+            var data = $(e.target).serializeArray(),
+                // create a new instance of Blog
+                blog = new blog();
+            // call .create()
+            blog.create(data[0].value, data[1].value);
+        },
+        render: function(e) {
+            this.$el.html(this.template());
+        }
+    });
+
     // render login view on page
     var loginView = new LoginView();
     loginView.render();
     $('.main-container').html(loginView.el);
-
-    // blog view
-    var AddBlogView = Parse.View.extend({
-        template: Handlebars.compile($('#add-tpl').html()),
-        render: function(){
-            this.$el.html(this.template());
-        }
-    });
 
 }());
